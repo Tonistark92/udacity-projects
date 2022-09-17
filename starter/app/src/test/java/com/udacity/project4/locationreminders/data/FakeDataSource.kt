d8d46b93ce0277220b2d8a1df67b04c863b8e365
Islam_Hany <86676102+Tonistark92@users.noreply.github.com>
2022-09-17 14:48:29 +0200

    more and more comments

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -22,12 +22,27 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()):
     // here we test the getReminders dao fun if the result is error so we use the result class util
     //if not we return Result.Success(ArrayList(reminders)) so it will return the list of  data
     // so test on the error case and the sucess
+    // the error case that i pointed to that the DAO fun getReminders may not happen for some reasons
+    // so i test it  if it work so use the Result.Success(ArrayList(reminders)) and get me the list
+    // and if not return error
+    // i used the shouldReturnError for both check cases the error and the one case that really return
+    // the list and both using the util class Result and it's inner classes
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         if (shouldReturnError){
             return Result.Error("Reminders not found", 404)
         }else{
             return return Result.Success(ArrayList(reminders))
         }
+        // is that what you wanted ?
+//        if(reminders.isEmpty()){
+        //or reminders.size()==0
+
+//            return  Result.Error("Reminders not found", 404)
+//        }
+//        else
+//        {
+//            return return Result.Success(ArrayList(reminders))
+//        }
     }
 
     // and here based on the boolean if in the begining we dont want it to return any thing and get error
