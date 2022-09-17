9ce9c7a3a6235bf84679e8b9442fc1152f5196c5
Islam_Hany <86676102+Tonistark92@users.noreply.github.com>
2022-09-17 20:10:12 +0200

    more and more and more explaining

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,15 +2,14 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import java.lang.Exception
 
 
 class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()): ReminderDataSource {
 
-    private var shouldReturnError = false
+    private var wantReturnError = false
 
     fun setShouldReturnError(shouldReturn: Boolean) {
-        this.shouldReturnError = shouldReturn
+        this.wantReturnError = shouldReturn
     }
     override suspend fun saveReminder(reminder: ReminderDTO) {
         reminders.add(reminder)
@@ -23,12 +22,19 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()):
     //if not we return Result.Success(ArrayList(reminders)) so it will return the list of  data
     // so test on the error case and the sucess
     // the error case that i pointed to that the DAO fun getReminders may not happen for some reasons
+    // the reasons -> that we need to check if the last stored data can be retrieved or
+    // to check if we can connect using teh dao-> getreminders or not may be there is problem with query
+    // or i used the wrong method and we need instromented test and unit test for reasons like
+    // for unit the device may use some installed sql locally and for each device may differe in sql versions
+    // this from the 8th video in the course
     // so i test it  if it work so use the Result.Success(ArrayList(reminders)) and get me the list
     // and if not return error
-    // i used the shouldReturnError for both check cases the error and the one case that really return
+    // i used the wantReturnError for both check cases the error and the one case that really return
     // the list and both using the util class Result and it's inner classes
+    // again is used wantReturnError for each check you can one at the time make it true and see the error
+    // and let is false and it will return the list
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        if (shouldReturnError){
+        if (wantReturnError){
             return Result.Error("Reminders not found", 404)
         }else{
             return return Result.Success(ArrayList(reminders))
@@ -51,7 +57,7 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()):
     // for Result.Success(reminder) to return the remainder
     //else we use the result's class inner class error  Result.Error("Reminder not found", 404)
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        if(shouldReturnError){
+        if(wantReturnError){
             return Result.Error("Error")
         }else{
             val reminder = reminders.find { it.id == id }
