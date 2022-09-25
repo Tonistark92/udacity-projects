847a541e6d8a415605dd84cfd0a0d7d8b3804340
Islam_Hany <86676102+Tonistark92@users.noreply.github.com>
2022-09-25 16:54:03 +0200

    FInal Commit

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,6 +4,10 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
+//In the real database may occur exception something went wrong with data or schema
+//So we use try/catch block to Handel this in the viewmodel but here in the fake data source fake
+// data so we need to return data and return error manually I used like a flag (using the boolean like switcher)
+// to switch between error and data mode we can't check on empty list we have test for the error empty data so should return an error
 class FakeDataSource(var tasks: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
     private var error = false
